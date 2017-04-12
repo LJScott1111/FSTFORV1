b40d5af2670fbc630b25cae322c7c7bda2c86e43
Shraddha Porwal <shraddhaporwal@gmail.com>
2017-04-12 15:02:10 +0530

    Login files added, Global TSS updated

---
+++ app/controllers/Auth/Login.js
@@ -0,0 +1,4 @@
+var nsLogin = {};
+
+nsLogin.init = function() {
+}();
--- app/controllers/Main/MainView.js
+++ app/controllers/Main/MainView.js
@@ -1,2 +1,18 @@
-// Arguments passed into this controller can be accessed via the `$.args` object directly or:
-var args = $.args;
+var nsMainView = {};
+
+nsMainView.postlayoutCall = function() {
+	// if (!Titanium.App.Properties.getString('token')) {
+	// TODO: Check if user is logged in
+	Alloy.createController('Auth/Login', {
+		callback : function() {
+			console.log('CALLBACK!');
+		}
+	}).getView().open();
+	// }
+	$.vwMain.removeEventListener('postlayout', nsMainView.postlayoutCall);
+	return;
+};
+
+nsMainView.init = function() {
+	$.vwMain.addEventListener('postlayout', nsMainView.postlayoutCall);
+}();
