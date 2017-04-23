a59fb83d9ffe881bf989ac95638eab5781838bf6
Front Expert <frontexport1987@gmail.com>
2017-04-23 18:11:51 +0200

    fixed that not fullscreen video on ios

--- app/lib/Utils.js
+++ app/lib/Utils.js
@@ -1,22 +1,25 @@
+var utils = {};
 
-exports.validateEmail = function(emailValue) {
+utils.validateEmail = function(emailValue) {
 	var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return emailRegex.test(emailValue);
 };
 
-exports.validatePassword = function(passValue) {
+utils.validatePassword = function(passValue) {
 	var passRegex = /^[A-Za-z]\w{5,14}$/;
 	return passRegex.test(passValue);
 };
 
-exports.validateZeroLength = function(string) {
+utils.validateZeroLength = function(string) {
 	return (string.trim().length != 0);
 };
 
-exports.getDateFormatted = function(date) {
+utils.getDateFormatted = function(date) {
 	var momentjs = require("alloy/moment");
 	var dateObj = momentjs(date);
 	var dateString = "";
 	dateString = momentjs(date).format('YYYY-MM-DD');
 	return dateString;
 };
+
+module.exports = utils;
