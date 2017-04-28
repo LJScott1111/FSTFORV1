0cf0e0f74ef4ab1175f5699c969b0c4860ae85a7
Shraddha Porwal <shraddhaporwal@gmail.com>
2017-04-28 12:51:07 +0530

    Menu, webviews and weblinks added

--- app/controllers/Main/Menu.js
+++ app/controllers/Main/Menu.js
@@ -1,5 +1,86 @@
+$.register.button.addEventListener('click', function() {
 
-$.logout.button.addEventListener('click', function() {
+	Ti.App.fireEvent('toggleMenu');
+	Alloy.Globals.openWindow("Misc/WebLinkView", {
+		url : "http://festforums.ticketmob.com/event.cfm?cart&id=153670",
+	}, true);
+});
+
+$.speakers.button.addEventListener('click', function() {
 
 	Ti.App.fireEvent('toggleMenu');
 });
+
+$.schedule.button.addEventListener('click', function() {
+
+	Ti.App.fireEvent('toggleMenu');
+});
+
+$.sponsors.button.addEventListener('click', function() {
+
+	Ti.App.fireEvent('toggleMenu');
+});
+
+$.attendees.button.addEventListener('click', function() {
+
+	Ti.App.fireEvent('toggleMenu');
+});
+
+$.newsSocial.button.addEventListener('click', function() {
+
+	Ti.App.fireEvent('toggleMenu');
+});
+
+$.santaBarbara.button.addEventListener('click', function() {
+
+	Ti.App.fireEvent('toggleMenu');
+	Alloy.Globals.openWindow("Misc/WebLinkView", {
+		url : "http://www.festforums.com/welcome-sb17/",
+	}, true);
+});
+
+$.pastEvents.button.addEventListener('click', function() {
+
+	Ti.App.fireEvent('toggleMenu');
+	Alloy.Globals.openWindow("Misc/WebLinkView", {
+		url : "http://www.festforums.com/past-events-1/",
+	}, true);
+});
+
+$.whoWeAre.button.addEventListener('click', function() {
+
+	Ti.App.fireEvent('toggleMenu');
+	Alloy.Globals.openWindow("Misc/WebLinkView", {
+		url : "http://www.festforums.com/team/",
+	}, true);
+});
+
+$.inTheMedia.button.addEventListener('click', function() {
+
+	Ti.App.fireEvent('toggleMenu');
+	Alloy.Globals.openWindow("Misc/WebLinkView", {
+		url : "http://www.festforums.com/media/",
+	}, true);
+});
+
+$.contact.button.addEventListener('click', function() {
+
+	Ti.App.fireEvent('toggleMenu');
+	Alloy.Globals.openWindow("Misc/WebLinkView", {
+		url : "http://www.festforums.com/contact/",
+	}, true);
+});
+
+$.privacyPolicy.button.addEventListener('click', function() {
+
+	Ti.App.fireEvent('toggleMenu');
+	Alloy.Globals.openWindow("Misc/WebLinkView", {
+		url : "http://www.festforums.com/privacy-policy",
+	}, true);
+});
+
+/*
+ $.logout.button.addEventListener('click', function() {
+
+ Ti.App.fireEvent('toggleMenu');
+ });*/
---
+++ app/controllers/Misc/WebLinkView.js
@@ -0,0 +1,7 @@
+var nsWebViewLink = {};
+
+nsWebViewLink.init = function() {
+
+	$.webLinkView.setUrl($.args.url);
+}();
+
