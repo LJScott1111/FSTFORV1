fb6f53f1900ac908804c3a27e3fee547723e1140
Front Expert <frontexport1987@gmail.com>
2017-04-30 19:37:42 +0200

    Revert utils.js file

--- app/lib/Utils.js
+++ app/lib/Utils.js
@@ -1,25 +1,22 @@
-var utils = {};
 
-utils.validateEmail = function(emailValue) {
+exports.validateEmail = function(emailValue) {
 	var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return emailRegex.test(emailValue);
 };
 
-utils.validatePassword = function(passValue) {
+exports.validatePassword = function(passValue) {
 	var passRegex = /^[A-Za-z]\w{5,14}$/;
 	return passRegex.test(passValue);
 };
 
-utils.validateZeroLength = function(string) {
+exports.validateZeroLength = function(string) {
 	return (string.trim().length != 0);
 };
 
-utils.getDateFormatted = function(date) {
+exports.getDateFormatted = function(date) {
 	var momentjs = require("alloy/moment");
 	var dateObj = momentjs(date);
 	var dateString = "";
 	dateString = momentjs(date).format('YYYY-MM-DD');
 	return dateString;
 };
-
-module.exports = utils;
